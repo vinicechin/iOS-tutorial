7c1ddcc76c4fba47dcf0a83e801f0d1ce0652f1f
Vinicius Cechin <vinicechin@gmail.com>
2018-04-08 15:27:29 -0300

    Section 2 Lecture 12
    Start of 'I am rich' Project

---
+++ I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Vinicius Cechin on 08/04/2018.
+//  Copyright © 2018 Vinicius Cechin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
