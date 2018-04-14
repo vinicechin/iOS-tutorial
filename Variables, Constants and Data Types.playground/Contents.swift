58caf6b62fde0ada17cca285c358c7f4d85dcef3
Vinicius Cechin <vinicechin@gmail.com>
2018-04-14 18:53:44 -0300

    Playground about variables, constants and data types

---
+++ Variables, Constants and Data Types.playground/Contents.swift
@@ -0,0 +1,19 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var myAge:Int = 30
+
+myAge = 29
+
+let myName:String = "Vinicius" + " Amaro"
+
+let name2 = "\(myName) Cechin"
+
+let details = "Name: \(name2), Age: \(myAge)"
+
+let boring:Bool = true
+
+let floatNumber:Float = 1.3
+
+let double:Double = 3.14159265359
