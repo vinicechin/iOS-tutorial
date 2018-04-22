d80f96e433dc8b5391a34aa70db190a996e8fa3c
Vinicius Cechin <vinicechin@gmail.com>
2018-04-22 13:15:06 -0300

    Linked code with design

--- Magic 8 Ball/Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/Magic 8 Ball/ViewController.swift
@@ -9,7 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var imageBall8: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +21,7 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func askAction(_ sender: Any) {
+    }
 }
 
