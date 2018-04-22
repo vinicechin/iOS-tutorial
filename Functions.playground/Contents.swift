0d520a499dc4a733abf64a43b1483e51c6fb630f
Vinicius Cechin <vinicechin@gmail.com>
2018-04-22 14:23:00 -0300

    Lecture 62 - functions part1

---
+++ Functions.playground/Contents.swift
@@ -0,0 +1,15 @@
+// function default
+func functionName() {
+    //...
+}
+
+// creator of function
+func getMilk() {
+    print("go to the market")
+    print("get 2 cartons of milk")
+    print("pay 2$ for the milk")
+    print("go home")
+}
+
+// caller of function
+getMilk()
