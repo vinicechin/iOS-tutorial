4062949c7dc680dced2592825c32297db5d93ab5
Vinicius Cechin <vinicechin@gmail.com>
2019-09-23 15:45:25 +0200

    Implemented remaining functionalities

--- Destini/ViewController.swift
+++ Destini/ViewController.swift
@@ -36,7 +36,7 @@ class ViewController: UIViewController {
     
     // TODO Step 5: Initialise instance variables here
     
-    
+    var storyIndex : Int = 1;
     
     
     override func viewDidLoad() {
@@ -44,7 +44,9 @@ class ViewController: UIViewController {
         
         
         // TODO Step 3: Set the text for the storyTextView, topButton, bottomButton, and to T1_Story, T1_Ans1, and T1_Ans2
-        
+        storyTextView.text = story1;
+        topButton.setTitle(answer1a, for: .normal)
+        bottomButton.setTitle(answer1b, for: .normal)
     }
 
     
@@ -52,6 +54,46 @@ class ViewController: UIViewController {
     @IBAction func buttonPressed(_ sender: UIButton) {
     
         // TODO Step 4: Write an IF-Statement to update the views
+        switch storyIndex {
+        case 1:
+            if (sender.tag == 1) {
+                storyIndex = 3
+                storyTextView.text = story3
+                topButton.setTitle(answer3a, for: .normal)
+                bottomButton.setTitle(answer3b, for: .normal)
+            } else {
+                storyIndex = 2
+                storyTextView.text = story2
+                topButton.setTitle(answer2a, for: .normal)
+                bottomButton.setTitle(answer2b, for: .normal)
+            }
+            break
+        case 2:
+            if (sender.tag == 1) {
+                storyIndex = 3
+                storyTextView.text = story3
+                topButton.setTitle(answer3a, for: .normal)
+                bottomButton.setTitle(answer3b, for: .normal)
+            } else {
+                storyTextView.text = story4
+                topButton.isHidden = true
+                bottomButton.isHidden = true
+            }
+            break
+        case 3:
+            if (sender.tag == 1) {
+                storyTextView.text = story6
+                topButton.isHidden = true
+                bottomButton.isHidden = true
+            } else {
+                storyTextView.text = story5
+                topButton.isHidden = true
+                bottomButton.isHidden = true
+            }
+            break
+        default:
+            break
+        }
                 
         // TODO Step 6: Modify the IF-Statement to complete the story
         
