7945860c03af8d2cf8ac98b9cb436bdf94d5b4b4
Vinicius Cechin <vinicechin@gmail.com>
2019-09-24 17:37:12 +0200

    Create project and init pods

---
+++ Classes and Objects/Classes and Objects/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Classes and Objects
+//
+//  Created by Gabriella Barbieri on 24/09/19.
+//  Copyright © 2019 Gabriella Barbieri. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
