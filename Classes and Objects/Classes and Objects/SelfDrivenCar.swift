2692393507c115a76e9d276f7858e562ce062d1b
Vinicius Cechin <vinicechin@gmail.com>
2019-09-25 15:32:05 +0200

    Inheritance

---
+++ Classes and Objects/Classes and Objects/SelfDrivenCar.swift
@@ -0,0 +1,13 @@
+//
+//  SelfDrivenCar.swift
+//  Classes and Objects
+//
+//  Created by Gabriella Barbieri on 25/09/19.
+//  Copyright © 2019 Gabriella Barbieri. All rights reserved.
+//
+
+import Foundation
+
+class SelfDrivenCar : Car {
+    
+}
