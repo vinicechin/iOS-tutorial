19a345a8f5919f93bb006b9f9d89bddd87646b4e
Vinicius Cechin <vinicechin@gmail.com>
2019-10-14 16:25:08 +0200

    Add user registration process and login segue

--- Flash Chat/RegisterViewController.swift
+++ Flash Chat/RegisterViewController.swift
@@ -6,7 +6,7 @@
 //
 
 import UIKit
-
+import Firebase
 
 class RegisterViewController: UIViewController {
 
@@ -28,15 +28,14 @@ class RegisterViewController: UIViewController {
 
   
     @IBAction func registerPressed(_ sender: AnyObject) {
-        
-
-        
-        //TODO: Set up a new user on our Firbase database
-        
-        
-
-        
-        
+        Auth.auth().createUser(withEmail: emailTextfield.text!, password: passwordTextfield.text!) { (result, error) in
+            if error != nil {
+                print(error!)
+            } else {
+                print("Registered")
+                self.performSegue(withIdentifier: "goToChat", sender: self)
+            }
+        }
     } 
     
     
