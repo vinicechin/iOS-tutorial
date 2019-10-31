6eff1876dc89b1b9db44a7c4fa8759fa6c1a1965
Vinicius Cechin <vinicechin@gmail.com>
2019-10-31 10:31:52 +0100

    Process image for data results from the Inception-V3 model

--- SeeFood/SeeFood/ViewController.swift
+++ SeeFood/SeeFood/ViewController.swift
@@ -28,15 +28,43 @@ class ViewController: UIViewController {
 extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     func setImagePicker() {
         imagePicker.delegate = self as UIImagePickerControllerDelegate & UINavigationControllerDelegate
-        imagePicker.sourceType = .camera // .photoLibrary
+        imagePicker.sourceType = .photoLibrary // .camera
         imagePicker.allowsEditing = false
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         if let pickedImage = info[.originalImage] as? UIImage {
             imageView.image = pickedImage
+            
+            guard let ciimage = CIImage(image: pickedImage) else {
+                fatalError("Could not convert UIImage into CIImage")
+            }
+            
+            detect(ciimage)
         }
         
         imagePicker.dismiss(animated: true, completion: nil)
+    }
+}
+
+extension ViewController {
+    func detect(_ image: CIImage) {
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
+            fatalError("Loading CoreML model failed")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let results = request.results as? [VNClassificationObservation] else {
+                fatalError("Model failed to process image")
+            }
+            
+            print(results)
+        }
+        
+        do {
+            try VNImageRequestHandler(ciImage: image).perform([request])
+        } catch {
+            print("Error performing request to model, \(error)")
+        }
     }
 }
